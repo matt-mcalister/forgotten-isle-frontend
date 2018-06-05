2a8b0592e20ee782c6ab80b210ee5519078c3ad1
github username <github email address>
2018-06-05 12:47:23 -0400

    updated http to https

--- src/connections/constants.js
+++ src/connections/constants.js
@@ -1,4 +1,4 @@
-export const API_ROOT = 'http://forgotten-isle-backend.herokuapp.com/api/v1';
+export const API_ROOT = 'https://forgotten-isle-backend.herokuapp.com/api/v1';
 export const API_WS_ROOT = 'wss://forgotten-isle-backend.herokuapp.com/api/v1/cable';
 
 export const HEADERS = () => {
